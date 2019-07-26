752d37369aa8266deb1790a756c6aafb5a2d5747
Matthew <mfarmer5102@gmail.com>
2019-07-26 19:32:24 -0400

    Fixed broken sending userID in record creation

--- frontend/src/components/entryAddForm.js
+++ frontend/src/components/entryAddForm.js
@@ -28,7 +28,6 @@ class EntryAddForm extends Component {
   clearForm() {
     this.setState({
       entry: {
-        userID: '',
         dueDate: '',
         taskDescription: ''
       }
@@ -39,7 +38,11 @@ class EntryAddForm extends Component {
   addEntry = (event) => {
       event.preventDefault();
       const entry = this.state.entry;
-      fetch(`http://localhost:4000/entries/add?userID=${entry.userID}&dueDate=${entry.dueDate}&taskDescription=${entry.taskDescription}`)
+
+      //The following will hard code the user's FirebaseID
+      let queryURL = `http://localhost:4000/entries/add?userID=${sessionStorage.getItem("mernTasksUserID")}&dueDate=${entry.dueDate}&taskDescription=${entry.taskDescription}`
+      console.log(queryURL)
+      fetch(queryURL)
           .catch(err => console.error(err))
       this.clearForm();
   }
@@ -52,17 +55,6 @@ class EntryAddForm extends Component {
         <div className="p-3 mb-3 rounded border" onClick={this.props.onClick}> {/*This is where the child component uses its inherited method; when this div is clicked, handleClick is performed on the parent component*/}
 
             <form>
-
-            <div class="form-group">
-                <label for="formGroupExampleInput">UserID</label>
-                <input
-                    className="form-control m-1"
-                    value={sessionStorage.getItem("mernTasksUserID")}
-                    onChange={event => this.setState({
-                      entry: { ...entryToAdd, userID: sessionStorage.getItem("mernTasksUserID")} //..Before the comma is the object to use, after the comma is the alteration to make to it
-                    })}
-                />
-              </div>
 
               <div class="form-group">
                 <label for="formGroupExampleInput">Due Date</label>
